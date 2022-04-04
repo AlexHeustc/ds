6fa85778c5f3b1ce57214a07ad5afddbe21a0d10
AlexHeustc <731681007@qq.com>
2022-04-04 23:48:49 +0800

    biotree

--- 二叉树/二叉树/data_structure.h
+++ 二叉树/二叉树/data_structure.h
@@ -1,43 +1,46 @@
-//定义二叉树表示方法
+//定义二叉树的表示方法
 typedef struct BTNode
 {
 	char data;
 	struct BTNode* pLchild;
 	struct BTNode* pRchild;
-}BTNode,*BTree;
+}BTNode, * BTree;
 
 
 //创建一棵二叉树
-BTree creat_tree();
+BTree create_tree();
 //非递归实现二叉树的前序遍历
 void pre_traverse(BTree);
-//非递归实现二叉树的中序遍历
+//非递归实现二叉树的俄中序遍历
 void in_traverse(BTree);
 //非递归实现二叉树的后序遍历
 void beh_traverse(BTree);
 
+
 //定义栈的表示方法
-typedef struct NODE
+typedef struct Node
 {
 	BTree data;
-	struct NODE *pNext;
-}BNODE,*PNODE;
+	struct Node* pNext;
+}NODE, * PNODE;
 
-typedef struct STACK
+typedef struct Stack
 {
 	PNODE pTop;
 	PNODE pBottom;
-}STACK,*PSTACK;
+}STACK, * PSTACK;
 
 //创建空栈的函数
 PSTACK create_stack();
 //入栈函数
-void push_stack(PSTACK, BTree);//BTree为数据的类型
+void push_stack(PSTACK, BTree);
 //遍历栈
 void traverse_stack(PSTACK);
-//判断栈空函数
+//出栈函数
+bool pop_stack(PSTACK, BTree*);
+//判断栈空函数 
 bool is_empty(PSTACK);
 //清空栈
 void clear_stack(PSTACK);
 //取栈顶元素
-BTree get_top(STACK);
+BTree getTop(PSTACK);
