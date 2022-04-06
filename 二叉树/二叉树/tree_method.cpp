da4924632129b9bf84e5d1ce0e00cabf50e82705
AlexHeustc <731681007@qq.com>
2022-04-06 11:58:54 +0800

    2

--- 二叉树/二叉树/tree_method.cpp
+++ 二叉树/二叉树/tree_method.cpp
@@ -44,6 +44,52 @@ void pre_traverse(BTree pTree)
 	//直到当前节点pCur为NULL且栈为空，循环结束
 	while (pCur || !is_empty(stack))
 	{
+		//从根节点开始，输出当前节点，并将其入栈，
+		//同时置其左孩子为当前节点，直至其没有左孩子，直到前节点为NULL
+		printf("%c", pCur->data);
+		push_stack(stack, pCur);
+		pCur = pCur->pLchild;
+		//如果当前节点pCur为NULL且栈不空，则将栈顶节点出栈，
+		//同时置其右孩子为当前节点,循环判断，直至pCur不为空
+		while (!pCur || !is_empty(stack))
+		{
+			pCur = getTop(stack);
+			pop_stack(stack, &node_pop);
+			pCur = pCur->pRchild;
+		}
+	}
+}
 
+/*
+中序的非递归实现
+*/
+void in_traverse(BTree pTree)
+{
+	PSTACK stack = create_stack();
+	BTree node_pop;
+	BTree pCur = pTree;
+
+	while (pCur || !is_empty(stack))
+	{
+		if (pCur->pLchild)
+		{
+			push_stack(stack, pCur->pLchild);
+			pCur = pCur->pLchild;
+		}
+		else
+		{
+			printf("%c", pCur->data);
+			pCur = pCur->pRchild;
+			//如果为空，且栈不空，则将栈顶节点出栈，并输出该节点，
+			//同时将它的右孩子设为当前节点，继续判断，直到当前节点不为空
+			while (!pCur && !is_empty(stack))
+			{
+				pCur = getTop(stack);
+				printf("%c", pCur->data);
+				pop_stack(stack, &node_pop);
+				pCur = pCur->pRchild;
+			}
+		}
+		
 	}
 }
